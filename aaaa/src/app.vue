cc0494f3fa8a016ef081ffaa8c37280b1063c473
fengyuxiu121 <1901535908@qq.com>
2019-11-20 20:29:52 +0800

    555

---
+++ aaaa/src/app.vue
@@ -0,0 +1,358 @@
+<template>
+	<div id="app">
+		<div class="bigbox">
+			<div class="head">
+				<h3>起飞页自助建站平台</h3>
+				<div class="head-con">
+					<p>云建站自动平台，<span>成就梦想</span></p>
+				</div>
+				<div class="head-bottom">
+					<p>自动建站平台</p>
+					<div class="tu">
+						<img class="img" v-for="i in arr" :src="i"/>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="content">
+			<!--<img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvMWYyYjI0N2JjYTc5MjhkYzIyOTRiMjc0M2UyZDI1MWQuanBn.jpg"/>-->
+			<div class="con">
+				<h3>您的创业梦想需要一个非同凡响的网站</h3>
+				<p>云建站倾力打造的一个从网站建设.网网站运营管理到网站推广的自动化免费建站平台</p>
+				<div class="bigyemian">
+					<div class="yemian">
+						<img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvZjgxMjkwZTg3YTEzM2ZkNWFiM2IzY2ExNjdhNTA3ZDgucG5n.png" alt="" />
+						<h3>页面要炫酷,颜值要高</h3>
+						<p>大家都金卡圣诞节哦闪客快打付款方 看奥斯卡施蒂利克大家都金卡圣诞节哦闪客快打付款方 看奥斯卡施蒂利克肯肯定是分开的啊叫啥</p>
+					</div>
+					<div class="yemian">
+						<img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvZjgxMjkwZTg3YTEzM2ZkNWFiM2IzY2ExNjdhNTA3ZDgucG5n.png" alt="" />
+						<h3>页面要炫酷,颜值要高</h3>
+						<p>大家都金卡圣诞节哦闪客快打付款方 看奥斯卡施蒂利克肯定是分大家都金卡圣诞节哦闪客快打付款方 看奥斯卡施蒂利克肯开的啊叫啥</p>
+					</div>
+					<div class="yemian">
+						<img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvZjgxMjkwZTg3YTEzM2ZkNWFiM2IzY2ExNjdhNTA3ZDgucG5n.png" alt="" />
+						<h3>页面要炫酷,颜值要高</h3>
+						<p>大家都金卡圣诞节哦闪客快打付款方 看奥斯卡施蒂利克肯定是分开的啊叫大家都金卡圣诞节哦闪客快打付款方 看奥斯卡施蒂利克肯啥</p>
+					</div>
+				</div>
+				<div class="con-con-con">
+					<h3>云建站自助平台</h3>
+					<p>上手快.颜值高.跨多屏.自传播.全免费.</p>
+				</div>
+				<div class="con-con-con2">
+					<p>在起飞页自助建站平台，您不需要任何编程基础，就能做出功能强大，效果出众的专业级网站，您所要做的仅是选择网站模板、输入图片文字，以及鼠标拖拉拽；真正的响应式建站，无论是在电脑的大屏幕，还是手机小屏幕上，网站都能友好地展示；您的访客还可以注册成为会员，并进行在线购物；当然，如果您需要多语言版本，起飞页也可以轻松为您实现。</p>
+				</div>
+			</div>
+		</div>
+		<div class="content2">
+				<h3>云建站，一站式的在线H5建站平台</h3>
+				<p>云建站倾力打造的一个从网站建设.网网站运营管理到网站推广的自动化免费建站平台</p>
+				<img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvNTE1Y2RhYTQxNDg0YWRiZDY4Mjg1NmU0OGNkZjFlYWYucG5n.png" alt="" />
+		</div>
+		<div class="content3">
+			<div class="content3-con">
+				<img src=""/>
+				<h3>跨屏展示</h3>
+				<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+			</div>
+			<div class="content3-con">
+				<img src=""/>
+				<h3>跨屏展示</h3>
+				<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+			</div>
+			<div class="content3-con">
+				<img src=""/>
+				<h3>跨屏展示</h3>
+				<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+			</div>
+			<div class="content3-con">
+				<img src=""/>
+				<h3>跨屏展示</h3>
+				<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+			</div>
+			<div class="content3-con">
+				<img src=""/>
+				<h3>跨屏展示</h3>
+				<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+			</div>
+			<div class="content3-con">
+				<img src=""/>
+				<h3>跨屏展示</h3>
+				<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+			</div>
+			<div class="content3-con">
+				<img src=""/>
+				<h3>跨屏展示</h3>
+				<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+			</div>
+			<div class="content3-con">
+				<img src=""/>
+				<h3>跨屏展示</h3>
+				<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+			</div>	
+		</div>
+		<div class="content4">
+			<h3>云建站，一站式的在线H5建站平台</h3>
+			<p>云建站倾力打造的一个从网站建设.网网站运营管理到网站推广的自动化免费建站平台</p>
+		</div>
+		
+		<!--ditu-->
+		<template>
+ 			<div>
+  <!-- <div id="container"></div> -->
+  		<div id="allmap"style="height: 500px;margin: auto;width: 80%;padding: 0 20px;"></div>
+ 		</div>
+		</template>
+		
+		
+	</div>
+</template>
+
+<script>
+	export default {
+  mounted() {
+   var map = new BMap.Map("allmap");
+   // var point = new BMap.Point(116.331398, 39.897445);北京的经纬度
+   map.addControl(new BMap.NavigationControl());
+   map.addControl(new BMap.ScaleControl());
+   map.addControl(new BMap.OverviewMapControl());
+   map.addControl(new BMap.MapTypeControl());
+   map.centerAndZoom('苏州市外河桥', 11);
+  
+ },
+		data(){
+		  return {
+		  	 arr:['https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvYjgxZjYwZjI5NWFlMzgzODExMzliNDU2ZTkwMGUyYTEtNDd4NDcucG5n.png',
+		'https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvODUyMjA2ZWViZDZiMmY0YjU1ZjkwZGI3MjNhODRjNWMtNDd4NDcucG5n.png',
+		  	 'https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvZjU4N2RiMTkzOWE0YjI4NTk0NzA3MjFjYTJhZTYwMDAtNDd4NDcucG5n.png',
+		  	 'https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvOWIxMTg4ODYwZDI4MzE0MTlmNjkyZjJhM2Y5ZTY5OGUtNDd4NDcucG5n.png',
+		  	 'https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvZDczNTgwOTlmZWEzN2UyY2E4YTVjZWVjNmIxMDU4NmUtNDd4NDcucG5n.png',
+		  	 'https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvZjcxMDlkMGU1YmUwZWQ1MjA5ZTk0YjFlYjMwMzY3YjYtNDd4NDcucG5n.png']
+		  }
+		},
+		methods: {
+			btn() {
+				alert('hello')
+			}
+		}
+	}
+</script>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+	#app{
+		width: 100%;
+		height: auto;
+		position: relative;
+	}
+	.bigbox{
+		width: 100%;
+		height: 708px;
+		z-index: 0;
+		overflow: hidden;
+		margin-bottom: 150px;
+		/*z-index: 1;*/
+		background-repeat: no-repeat;
+		background-size: cover;
+		/*position: fixed;*/
+		background-image: url('https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDMvNzIzNGQ1MjBkZjBiODhiMDIwYTc3ZmYwZjk1MGM3OGEuanBn.jpg');
+	}
+	.head{
+		width: 1259px;
+		height: 385px;
+		margin: auto;
+		margin-top: 184px;
+	}
+	.head h3{
+		font-size: 60px;
+		color: #ffffff;
+		margin-bottom: 30px;
+		text-align: center;
+	}
+	.head-con{
+		width: 700px;
+		height: 100px;
+		margin: auto;
+		border-bottom: 1px solid #FFFFFF;
+		border-top: 1px solid #FFFFFF;
+		background: rgba(255,255,255, .3);
+		margin-bottom: 20px;
+	}
+	.head-con p{
+		font-size: 30px;
+		color: #FFFFFF;
+		line-height: 100px;
+		text-align: center;
+	}
+	.head-con p span{
+		color: #e84c3d;
+	}
+	.head-bottom{
+		width: 300px;
+		height: 110px;
+		margin: auto;
+	}
+	.head-bottom p{
+		font-size: 35px;
+		color: #FFFFFF;
+		margin-bottom: 15px;
+		text-align: center;
+	}
+	.img{
+		width: 39px;
+		height: 39px;
+		margin: 4px;
+	}
+	.content{
+		width: 100%;
+		height: 708px;
+		background-image: url('https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvMWYyYjI0N2JjYTc5MjhkYzIyOTRiMjc0M2UyZDI1MWQuanBn.jpg');
+		background-repeat: no-repeat;
+		background-size: cover;
+		position: relative;
+		/*position: fixed;*/
+	}
+	.con{
+		width: 100%;
+		height: 600px;
+		background: #FFFFFF;
+		position: absolute;
+		top:-300px;
+		left: 0px;
+	}
+	.con h3{
+		font-size: 40px;
+		text-align: center;
+		color: #E84C3D;
+		margin-top: 50px;
+		margin-bottom: 30px;
+	}
+	.con p{
+		font-size: 21px;
+		text-align: center;
+		color: #cccccc;
+	}
+	.bigyemian{
+		width: 100%;
+		height: 362px;
+		
+	}
+	.yemian{
+		margin: 60px;
+		height: 350px;
+		width: 320px;
+		float: left;
+	}
+	.yemian img{
+		width: 106px;
+		height: 101px;
+		margin-left: 120px;
+		margin-bottom: 15px;
+	}
+	.yemian h3{
+		font-size: 21px;
+		color: #CCCCCC;
+		margin-bottom: 15px;
+	}
+	.yemian p{
+		font-size: 18px;
+		color: #CCCCCC;
+	}
+	.con-con-con{
+		width: 100%;
+		height: 190px;
+		margin-bottom: 40px;
+		background: rgba(255,255,255, .3);
+	}
+	.con-con-con h3{
+		font-size: 28px;
+		text-align: center;
+		color: #FFFFFF;
+		line-height: 1;
+		margin-bottom: 8px;
+		/*margin-top: 10px;*/
+	}
+	.con-con-con p{
+		font-size: 28px;
+		color: #E84C3D;
+	}
+	.con-con-con2{
+		width: 810px;
+		height: 112px;
+		margin: auto;
+	}
+	.con-con-con2 p{
+		text-align: center;
+		color: #FFFFFF;
+		font-size: 18px;
+	}
+	.content2{
+		width: 100%;
+		height: 200px;
+	}
+	.content2 h3{
+		font-size: 40px;
+		color: #E84C3D;
+		text-align: center;
+		margin-top: 30px;
+		margin-bottom: 40px;
+	}
+	.content2 p{
+		font-size: 18px;
+		text-align: center;
+		color: #CCCCCC;
+		margin-bottom: 40px;
+	}
+	.content2 img{
+		width: 760px;
+		height: 646px;
+		margin-left:38340px;
+	}
+	.content3{
+		width: 100%;
+		height: 526px;
+	}
+	.content3-con{
+		width: 250px;
+		height: 250px;
+		margin: 40px;
+		float: left;
+	}
+	.content3-con img{
+		width: 49px;
+		height: 45px;
+		margin-left: 100px;
+	}
+	.content3-con h3{
+		font-size: 21px;
+		color: #FFFFFF;
+		margin: 10px;
+		text-align: center;
+	}
+	.content3-con p{
+		font-size: 14px;
+		color: #CCCCCC;
+		margin: 10px;
+		text-align: center;
+	} 
+	.content4{
+		width: 100%;
+		height: 1000px;
+	}
+	.content4 h3{
+		font-size: 40px;
+		color: #E84C3D;
+		margin-bottom: 30px;
+		text-align: center;
+	}
+	.content4 p{
+		font-size: 20px;
+		color: #CCCCCC;
+		text-align: center;
+		margin-bottom: 30px;
+	}
+</style>
